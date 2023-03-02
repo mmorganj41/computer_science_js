dd7326e00f09a2928e53f37f9276e92d3f6b9060
mmorganj41 <mmorganj41@gmail.com>
2023-03-02 15:22:50 -0500

    make quicksort

--- divide-sorting/QuickSort.js
+++ divide-sorting/QuickSort.js
@@ -1,5 +1,20 @@
-function quickSort(arr){
+function quickSort(arr) {
   // YOUR CODE HERE
+  if (arr.length <= 1) return arr;
+
+  const less = [];
+  const pivot = arr[0];
+  const greater = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      less.push(arr[i]);
+    } else {
+      greater.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(less), pivot, ...quickSort(greater)];
 }
 
 module.exports = quickSort;
