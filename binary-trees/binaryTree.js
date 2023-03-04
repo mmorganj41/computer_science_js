f2a7779284d025a82e145fb8f8f99c5e132b4553
mmorganj41 <mmorganj41@gmail.com>
2023-03-03 20:19:18 -0500

    make binary tree

--- binary-trees/binaryTree.js
+++ binary-trees/binaryTree.js
@@ -1,36 +1,141 @@
 class BinaryNode {
-    constructor(){
-    // a node has data, left, and right pointers
-    // left and right are intialized as null
+    constructor(data) {
+        // a node has data, left, and right pointers
+        // left and right are intialized as null
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
 }
 class BinaryTree {
-    constructor(){
+    constructor() {
         // when a new Tree is made, it has a root property
+        this.root = null;
     }
-    insert(data){
+    insert(data) {
         // add a new Node to the tree, with data as the Node's data
         // if the data is already in the tree, do not insert it
+        if (this.root) {
+            helperInsert(this.root, data);
+        } else {
+            this.root = new BinaryNode(data);
+        }
+
+
+        function helperInsert(node, data) {
+            if (data < node.data) {
+                if (node.left) {
+                    helperInsert(node.left, data);
+                } else {
+                    node.left = new BinaryNode(data);
+                }
+            } else if (data > node.data) {
+                if (node.right) {
+                    helperInsert(node.right, data);
+                } else {
+                    node.right = new BinaryNode(data);
+                }
+            }
+        }
     }
-    search(val){
+    search(val) {
         // search the Tree for a node with the given value
         // if the node exists, return it
         // if the node doesn't exist, return false
+        if (!this.root) return false;
+
+        return helperSearch(this.root, val);
+
+        function helperSearch(node, val) {
+            if (val === node.data) {
+                return true;
+            } else if (val < node.data) {
+                if (node.left) return helperSearch(node.left, val);
+            } else if (val > node.data) {
+                if (node.right) return helperSearch(node.right, val);
+            }
+
+            return false;
+        }
     }
-    size(node){
+    size(node) {
         // calculate the number of nodes in the tree, starting from the given node
+        let count = 0;
+        if (!this.root) return count;
+
+        helperSizer(this.root);
+
+        return count;
+
+        function helperSizer(node) {
+            count++;
+
+            if (node.left) helperSizer(node.left);
+
+            if (node.right) helperSizer(node.right);
+        }
     }
-    getMax(){
+    getMax() {
         // return the maximum value stored in the tree
+
+        if (!this.root) return null;
+
+        return helperGetMax(this.root);
+
+        function helperGetMax(node) {
+            if (node.right) {
+                return helperGetMax(node.right);
+            } else {
+                return node.data;
+            }
+        }
     }
-    height(node){
+    height(node = this.root) {
         // calculate the maximum amount of nodes in any one path from the given node
         // if not given a specific node, default to using the root node
+
+        if (!node) return 0;
+
+        return 1 + helperCheckDepth(node);
+
+        function helperCheckDepth(node) {
+            let left = 0;
+            let right = 0;
+
+            if (node.left) left = 1 + helperCheckDepth(node.left);
+
+            if (node.right) right = 1 + helperCheckDepth(node.right);
+
+            return Math.max(left, right);
+        }
     }
-    isBalanced(node){
+    isBalanced(node = this.root) {
         // return true or false based on whether the sub-tree starting at the given node is balanced
         // A tree is imbalanced if the height of one branch exceeds the other side by more than one level
         // A tree is balanced if all branches end within one level of each other.
+
+        if (!node) return false;
+
+        return helperCheckDifference(node, true);
+
+        function helperCheckDifference(node, bothLimbs) {
+
+            if (!node.left && !node.right) {
+                return true;
+            } else if (node.left && node.right) {
+                return helperCheckDifference(node.left, true) && helperCheckDifference(node.right, true);
+            } else {
+                if (bothLimbs) {
+                    if (node.right) {
+                        return helperCheckDifference(node.right, false);
+                    } else {
+                        return helperCheckDifference(node.left, false);
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
     }
 }
 
--- binary-trees/test/BinaryTree.js
+++ binary-trees/test/BinaryTree.js
@@ -1,28 +1,28 @@
 const expect = require('chai').expect;
 const { BinaryNode, BinaryTree } = require('../binaryTree');
 
-describe('BinaryNode', ()=>{
-    it('should initialize with null left and right pointers', ()=>{
+describe('BinaryNode', () => {
+    it('should initialize with null left and right pointers', () => {
         const newNode = new BinaryNode(5);
         expect(newNode.left).to.equal(null);
         expect(newNode.right).to.equal(null);
     })
 })
 
-describe('BinaryTree', ()=>{
-    it('should initialize with a null root', ()=>{
+describe('BinaryTree', () => {
+    it('should initialize with a null root', () => {
         const tree = new BinaryTree();
         expect(tree.root).to.equal(null);
         expect(tree.root).to.not.be.undefined;
     });
-    it('should place the first node as the root', ()=>{
+    it('should place the first node as the root', () => {
         const tree = new BinaryTree();
         tree.insert(5);
         expect(tree.root.data).to.equal(5);
         expect(tree.root.left).to.equal(null);
         expect(tree.root.right).to.equal(null);
     });
-    it('should place new nodes according to Binary Tree rules', ()=>{
+    it('should place new nodes according to Binary Tree rules', () => {
         const tree = new BinaryTree();
         tree.insert(5);
         tree.insert(7);
@@ -39,7 +39,7 @@ describe('BinaryTree', ()=>{
         tree.insert(6);
         expect(tree.root.right.left.data).to.equal(6);
     });
-    it('should return true when searching for existing values', ()=>{
+    it('should return true when searching for existing values', () => {
         const tree = new BinaryTree();
         tree.insert(5);
         tree.insert(7);
@@ -49,35 +49,35 @@ describe('BinaryTree', ()=>{
         tree.insert(6);
         tree.insert(2);
         tree.insert(4);
-        for(let i = 1; i < 9; i++){
+        for (let i = 1; i < 9; i++) {
             expect(tree.search(i)).to.be.true;
         }
     });
-    it('should return false when searching for non-existent values', ()=>{
+    it('should return false when searching for non-existent values', () => {
         const tree = new BinaryTree();
-        for(let i = 1; i < 9; i++){
+        for (let i = 1; i < 9; i++) {
             tree.insert(i);
         }
         expect(tree.search(9)).to.be.false;
     });
-    it('should calculate the size accurately', ()=>{
+    it('should calculate the size accurately', () => {
         const tree = new BinaryTree();
         expect(tree.size(tree.root)).to.equal(0);
-        for(let i = 1; i < 9; i++){
+        for (let i = 1; i < 9; i++) {
             tree.insert(i);
         }
         expect(tree.size(tree.root)).to.equal(8);
     });
-    it('should return the maximum value with getMax', ()=>{
+    it('should return the maximum value with getMax', () => {
         const tree = new BinaryTree();
-        for(let i = 1; i < 9; i++){
+        for (let i = 1; i < 9; i++) {
             tree.insert(i);
         }
         expect(tree.getMax()).to.equal(8);
         tree.insert(9001);
         expect(tree.getMax()).to.equal(9001);
     });
-    it('should return the correct height from a given node', ()=>{
+    it('should return the correct height from a given node', () => {
         const tree = new BinaryTree();
         tree.insert(1);
         expect(tree.height()).to.equal(1);
@@ -89,5 +89,17 @@ describe('BinaryTree', ()=>{
         expect(tree.height(tree.root.right)).to.equal(2);
         expect(tree.height(tree.root.right.right)).to.equal(1);
         expect(tree.height(tree.root.left)).to.equal(0);
+    });
+    it('should return the correct balance from a given node', () => {
+        const tree = new BinaryTree();
+        tree.insert(1);
+        expect(tree.isBalanced()).to.equal(true);
+        tree.insert(3);
+        expect(tree.isBalanced()).to.equal(true);
+        expect(tree.isBalanced(tree.root.right)).to.equal(true);
+        tree.insert(5);
+        expect(tree.isBalanced()).to.equal(false);
+        expect(tree.isBalanced(tree.root.right)).to.equal(true);
+        expect(tree.isBalanced(tree.root.right.right)).to.equal(true);
     });
 })
