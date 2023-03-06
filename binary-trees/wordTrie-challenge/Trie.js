3359316c0336a4ba1ee73370113ea1053f3cff9c
mmorganj41 <mmorganj41@gmail.com>
2023-03-05 20:24:43 -0500

    word Trie

--- binary-trees/wordTrie-challenge/Trie.js
+++ binary-trees/wordTrie-challenge/Trie.js
@@ -1,5 +1,5 @@
 class trieNode {
-    constructor(data, isWord = false){
+    constructor(data, isWord = false) {
         this.data = data;
         this.isWord = isWord
         this.children = [];
@@ -7,27 +7,111 @@ class trieNode {
 }
 
 class wordTrie {
-    constructor(){
+    constructor() {
         this.root = new trieNode("");
     }
-    addWord(word){
+    addWord(word) {
         // this method accepts a complete word in string format
         // it adds a node for each letter if that node doesn't already exist in the Trie
         // each letter should reference the next node in its children array
+        word = word.toLowerCase();
+
+        let node = this.root;
+        for (const [index, letter] of Array.from(word).entries()) {
+            let childIndex = letter.charCodeAt(0) - 97;
+
+            if (node.children[childIndex]) {
+                node = node.children[childIndex];
+                if (index === word.length - 1) node.isWord = true;
+            } else {
+                const newNode = new trieNode(letter, index === word.length - 1);
+                node.children[childIndex] = newNode;
+                node = newNode;
+            }
+        }
     }
-    isWord(word){
+    isWord(word) {
         // this method accepts a string
         // return true if the string is a complete word
         // return false if the string is not a word
+        word = word.toLowerCase();
+        let node = this.root;
+        console.log(word);
+
+        for (const [index, letter] of Array.from(word).entries()) {
+            let childIndex = letter.charCodeAt(0) - 97;
+
+            if (!node.children[childIndex]) return false;
+
+            node = node.children[childIndex];
+            if (index === word.length - 1 && node.isWord) return true;
+        }
+        return false;
     }
-    autoComplete(str){
+    autoComplete(str) {
         // this method takes in a string representing what a user has typed so far
         // return an array of possible words that could complete the string given
+        str = str.toLowerCase();
+
+        let node = this.root;
+        const output = [];
+
+        for (const letter of str) {
+            let childIndex = letter.charCodeAt(0) - 97;
+
+            if (!node.children[childIndex]) return output;
+
+            node = node.children[childIndex];
+        }
+
+        if (node.isWord) output.push(str);
+
+        nextLetterHelper(node, str);
+
+        return output;
+
+        function nextLetterHelper(node, word) {
+            for (const nextNode of node.children) {
+                if (!nextNode) continue;
+                let newWord = word + nextNode.data;
+                if (nextNode.isWord) output.push(newWord);
+                nextLetterHelper(nextNode, newWord);
+            }
+        }
     }
-    bestWord(arrayOfLetters){
+    bestWord(arrayOfLetters) {
         // this method accepts an array of single character letters
         // using the array, find the largest word in the Trie that uses the combination of letters in the given array
         // use only the letters in the array, i.e. if the array contains two 's' letters, the best word can only have two 's' letters total
+
+        const possibleWords = [];
+
+        letterHelper('', this.root, arrayOfLetters);
+
+        return possibleWords.reduce((a, e) => {
+            if (e.length > a.length) return e;
+
+            return a;
+        }
+        );
+
+        function letterHelper(str, node, array) {
+            if (!array?.length) return;
+
+            for (const [index, letter] of array.entries()) {
+                const newNode = node.children[letter.charCodeAt(0) - 97];
+
+                if (!newNode) continue;
+
+                const newArray = [...array];
+                newArray.splice(index, 1);
+                const newStr = str + letter;
+
+                if (newNode.isWord) possibleWords.push(newStr);
+
+                letterHelper(newStr, newNode, newArray);
+            }
+        }
     }
 }
 
@@ -38,5 +122,9 @@ const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n');
 
 const theDictionary = new wordTrie();
 // loop over wordArray and add each letter to theDictionary
+
+wordArray.forEach(e => {
+    theDictionary.addWord(e);
+})
 
 module.exports = theDictionary;
--- binary-trees/wordTrie-challenge/test/index.js
+++ binary-trees/wordTrie-challenge/test/index.js
@@ -1,16 +1,16 @@
 const expect = require('chai').expect;
-const { theDictionary } = require('../Trie');
+const theDictionary = require('../Trie');
 
-describe('theDictionary', ()=>{
-    it('should confirm words that exist', ()=>{
+describe('theDictionary', () => {
+    it('should confirm words that exist', () => {
         let isWord = theDictionary.isWord("dog");
         expect(isWord).to.be.true;
     });
-    it('should return false on isWord for nonsense words', ()=>{
+    it('should return false on isWord for nonsense words', () => {
         let isWord = theDictionary.isWord("oneofusgooblegobblegooblegobble");
         expect(isWord).to.be.false;
     });
-    it('should give reasonable autoComplete suggestions', ()=>{
+    it('should give reasonable autoComplete suggestions', () => {
         let suggestions = theDictionary.autoComplete("pizz");
         expect(suggestions.includes("pizza")).to.be.true;
         let moreSuggestions = theDictionary.autoComplete("pre");
